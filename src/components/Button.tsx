86236c080596e57a9ae0c6aca92dc4eaf3d742d3
VladanS10 <vladan.nish@gmail.com>
2024-11-04 02:58:12 +0100

    commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,54 @@
+import { CSSProperties, PropsWithChildren } from "react";
+
+type ButtonProps = PropsWithChildren<{
+  onClick?: () => void;
+  variant: "primary" | "secondary" | "error";
+  size: "sm" | "md" | "lg";
+}>;
+
+export const Button = ({
+  children,
+  onClick,
+  variant = "primary",
+  size = "md",
+}: ButtonProps) => {
+  const variantStyles: Record<ButtonProps["variant"], CSSProperties> = {
+    primary: {
+      backgroundColor: "blue",
+    },
+    secondary: {
+      backgroundColor: "gray",
+    },
+    error: {
+      backgroundColor: "red",
+    },
+  };
+
+  const sizeStyles: Record<ButtonProps["size"], CSSProperties> = {
+    sm: {
+      padding: "0.5rem",
+    },
+    md: {
+      padding: "0.75rem",
+    },
+    lg: {
+      padding: "1rem",
+    },
+  };
+
+  return (
+    <button
+      style={{
+        outline: "none",
+        border: "none",
+        cursor: "pointer",
+        borderRadius: 10,
+        ...variantStyles[variant],
+        ...sizeStyles[size],
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,67 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { ComponentProps } from "react";
+
+import { Button } from "../components/Button";
+
+type StoryProps = ComponentProps<typeof Button> & {
+  buttonText: string;
+};
+
+const meta: Meta<StoryProps> = {
+  component: Button,
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      options: ["primary", "secondary", "error"],
+      control: {
+        type: "select",
+      },
+    },
+    size: {
+      options: ["sm", "md", "lg"],
+      control: {
+        type: "select",
+      },
+    },
+  },
+  args: {
+    onClick: fn(),
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<StoryProps>;
+
+export const Primary: Story = {
+  args: {
+    buttonText: "Hello",
+    variant: "primary",
+    size: "md",
+  },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    buttonText: "Hello",
+    variant: "secondary",
+    size: "md",
+  },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
+};
+export const Error: Story = {
+  args: {
+    buttonText: "Hello",
+    variant: "error",
+    size: "md",
+  },
+  render: ({ buttonText, ...args }) => {
+    return <Button {...args}>{buttonText}</Button>;
+  },
+};
